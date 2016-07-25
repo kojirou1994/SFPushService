1232cb3ac2f3bd7ac21069c4d9b043675cfcb53a
Kojirou <kojiroujyy@vip.qq.com>
2016-07-25 17:36:51 +0800

    add dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 let package = Package(
     name: "SFPushService",
     dependencies:[
-        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", versions: Version(0,0,0)..<Version(10,0,0))
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-Notifications.git", versions: Version(0,0,0)..<Version(10,0,0)),
+        .Package(url: "http://git.sfdai.com/Kojirou1994/SFMongo.git", versions: Version(0,0,0)..<Version(10,0,0)),
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0)),
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-MongoDB.git", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
