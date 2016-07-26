f9b54ca86cf40cd75bf6dfa2966aaff5658657b0
Kojirou <kojiroujyy@vip.qq.com>
2016-07-26 13:26:30 +0800

    Notification Model

--- Sources/Notification.swift
+++ Sources/Notification.swift
@@ -10,15 +10,23 @@ import Foundation
 import SFMongo
 import Dispatch
 
-enum App {
-    case loan
-    case aa
+enum App: Int, BSONStringConvertible, JSONStringConvertible {
+    case 蜜蜂聚财 = 100
+    case 蜜蜂易车贷 = 101
+    
+    var bsonString: String {
+        return self.rawValue.description
+    }
+    
+    var jsonString: String {
+        return self.rawValue.description
+    }
 }
 
 struct Notification: SFModel {
     var _id: ObjectId
     
-    var userToken: [String]
+    var userToken: String
     
     var app: App
     
@@ -28,7 +36,33 @@ struct Notification: SFModel {
     
     var badge: Int
     
+    var success: Bool
+    
+    var time: Date
+    
     init(json: JSON) throws {
-        <#code#>
+        guard let id = json["_id"].oid, token = json["userToken"].string, app = App(rawValue: json["app"].intValue), title = json["title"].string, body = json["body"].string, badge = json["badge"].int, success = json["success"].bool,time = json["time"].date else {
+            throw SFMongoError.invalidData
+        }
+        self._id = id
+        self.userToken = token
+        self.app = app
+        self.title = title
+        self.body = body
+        self.badge = badge
+        self.success = success
+        self.time = time
+    }
+}
+
+extension Notification {
+    init(userToken: String, app: App, title: String, body: String, badge: Int, time: Date) {
+        self._id = ObjectId.generate()
+        self.userToken = userToken
+        self.title = title
+        self.body = body
+        self.app = app
+        self.success = false
+        self.time = time
     }
 }
